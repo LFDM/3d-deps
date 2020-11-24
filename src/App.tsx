6789d821a19d68810a32c2c3de008294e7dd6684
LFDM <1986gh@gmail.com>
2020-11-24 19:30:11 +0100

    Show labels on hover

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { ForceGraph3D } from "react-force-graph";
 import "./App.css";
 import { toGraphData } from "./services/madge";
-import { GraphData } from "./types/GraphData";
+import { GraphData, IGraphNode } from "./types/GraphData";
 
 const Graph = ({ d }: { d: GraphData }) => {
-  return <ForceGraph3D graphData={d} nodeId="id" nodeAutoColorBy="group" />;
+  return (
+    <ForceGraph3D
+      graphData={d}
+      nodeId="id"
+      nodeAutoColorBy="group"
+      nodeLabel={(node) => (node as IGraphNode).id}
+    />
+  );
 };
 
 function App() {
