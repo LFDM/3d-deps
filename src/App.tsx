cfa77256dbad47aa748597b4e1447168575ca608
LFDM <1986gh@gmail.com>
2020-11-24 21:30:00 +0100

    Recolor

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import "./App.css";
 import { toGraphData } from "./services/madge";
 import { GraphData, IGraphNode } from "./types/GraphData";
 
 const Graph = ({ d }: { d: GraphData }) => {
-  const [nodes, setNodes] = useState(d.nodes);
+  const [i, setI] = useState(0);
   useEffect(() => {
     setInterval(() => {
-      const i = Math.floor(Math.random() * d.nodes.length);
-      const node = d.nodes[i];
-      console.log(node); // remove this line and fast-refresh to get a cool explosion!
-      node.color = "red";
-      setNodes([...d.nodes]);
-    }, 1000);
+      setI(Math.floor(Math.random() * d.nodes.length));
+    }, 500);
   }, []);
+  const toColor = useCallback(
+    (node: any) => {
+      if (d.nodes[i] === node) {
+        return "red";
+      }
+      return "";
+    },
+    [i, d.nodes]
+  );
   return (
     <ForceGraph3D
-      graphData={{
-        nodes,
-        links: d.links,
-      }}
+      graphData={d}
       nodeId="id"
+      nodeColor={toColor}
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
       nodeLabel={(node) => (node as IGraphNode).id}
