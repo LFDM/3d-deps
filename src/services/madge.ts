1b45b38576216668e0c784bddc9c8f06bb2abf96
LFDM <1986gh@gmail.com>
2020-11-24 19:02:58 +0100

    Map madge dependencies to graph data

---
+++ src/services/madge.ts
@@ -0,0 +1,21 @@
+import { GraphData, IGraphLink, IGraphNode } from "../types/GraphData";
+import * as DEPS from "./dependencies.json";
+
+export const toGraphData = (): GraphData => {
+  const nodes: IGraphNode[] = [];
+  const links: IGraphLink[] = [];
+  Object.entries(DEPS).forEach(([k, vs]) => {
+    const node: IGraphNode = {
+      id: k,
+      label: k,
+    };
+    vs.forEach((v) => {
+      const link: IGraphLink = {
+        source: v,
+        target: k,
+      };
+      links.push(link);
+    });
+  });
+  return { nodes, links };
+};
