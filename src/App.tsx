161248f4b7ad52e814ca3f21859234166bd639f9
LFDM <1986gh@gmail.com>
2020-11-24 21:17:41 +0100

    EXPLOSION EFFECT

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import "./App.css";
 import { toGraphData } from "./services/madge";
 import { GraphData, IGraphNode } from "./types/GraphData";
 
 const Graph = ({ d }: { d: GraphData }) => {
+  const [nodes, setNodes] = useState(d.nodes);
+  useEffect(() => {
+    setInterval(() => {
+      const i = Math.floor(Math.random() * d.nodes.length);
+      const node = d.nodes[i];
+      console.log(node); // remove this line and fast-refresh to get a cool explosion!
+      node.color = "red";
+      setNodes([...d.nodes]);
+    }, 1000);
+  }, []);
   return (
     <ForceGraph3D
-      graphData={d}
+      graphData={{
+        nodes,
+        links: d.links,
+      }}
       nodeId="id"
-      nodeAutoColorBy="group"
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
       nodeLabel={(node) => (node as IGraphNode).id}
