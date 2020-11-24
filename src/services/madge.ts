65d9920bbbcba4de67c5fe262f3161afba3e88ce
LFDM <1986gh@gmail.com>
2020-11-24 22:55:31 +0100

    Implement true madge handling

--- src/services/madge.ts
+++ src/services/madge.ts
@@ -1,28 +1,56 @@
+// @ts-ignore
+import * as madge from "madge";
 import { IDependencyAnalyzer } from "../types/DependencyAnalyzer";
 import { IGraphLink, IGraphNode } from "../types/GraphData";
 import DEPS from "./dependencies.json";
 
-export class MadgeAnalyzer implements IDependencyAnalyzer {
-  async analyze() {
-    const nodes: IGraphNode[] = [];
-    const links: IGraphLink[] = [];
-    Object.entries(DEPS).forEach(([k, vs]) => {
-      const node: IGraphNode = {
-        id: k,
-        label: k,
+type MadgeAnalyzerConfig = {
+  entry: string | string[];
+
+  baseDir?: string;
+  includeNpm?: boolean;
+  fileExtensions?: string[];
+  excludeRegExp?: string;
+  requireConfig?: string;
+  webpackConfig?: string;
+  tsConfig?: string;
+};
+
+const dependenciesToGraphData = (deps: { [key: string]: string[] }) => {
+  const nodes: IGraphNode[] = [];
+  const links: IGraphLink[] = [];
+  Object.entries(deps).forEach(([k, vs]) => {
+    const node: IGraphNode = {
+      id: k,
+      label: k,
+    };
+    nodes.push(node);
+    vs.forEach((v) => {
+      if (v.includes("node_modules")) {
+        return;
+      }
+      const link: IGraphLink = {
+        source: k,
+        target: v,
       };
-      nodes.push(node);
-      vs.forEach((v) => {
-        if (v.includes("node_modules")) {
-          return;
-        }
-        const link: IGraphLink = {
-          source: k,
-          target: v,
-        };
-        links.push(link);
-      });
+      links.push(link);
     });
-    return { nodes, links };
+  });
+  return { nodes, links };
+};
+
+export class MadgeAnalyzer implements IDependencyAnalyzer {
+  private config: MadgeAnalyzerConfig;
+  constructor(config: MadgeAnalyzerConfig) {
+    this.config = config;
+  }
+
+  async analyze() {
+    if (process.env.NODE_ENV === "development") {
+      return dependenciesToGraphData(DEPS);
+    }
+    const { entry, ...config } = this.config;
+    const deps = await madge(entry, config).then((r: any) => r.obj());
+    return dependenciesToGraphData(deps);
   }
 }
