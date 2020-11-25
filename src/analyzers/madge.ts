6c595c7e926d24f0ebb3b6f35a8797b2bd50fa46
LFDM <1986gh@gmail.com>
2020-11-25 06:34:26 +0100

    Clneaup madge analyzer

--- src/analyzers/madge.ts
+++ src/analyzers/madge.ts
@@ -4,31 +4,7 @@ import {
   DependencyNode,
   IDependencyAnalyzer,
 } from "../types/DependencyAnalyzer";
-import { IGraphLink, IGraphNode } from "../types/GraphData";
 import DEPS from "./dependencies.json";
-
-const dependenciesToGraphDataOld = (deps: { [key: string]: string[] }) => {
-  const nodes: IGraphNode[] = [];
-  const links: IGraphLink[] = [];
-  Object.entries(deps).forEach(([k, vs]) => {
-    const node: IGraphNode = {
-      id: k,
-      label: k,
-    };
-    nodes.push(node);
-    vs.forEach((v) => {
-      if (v.includes("node_modules")) {
-        return;
-      }
-      const link: IGraphLink = {
-        source: k,
-        target: v,
-      };
-      links.push(link);
-    });
-  });
-  return { nodes, links };
-};
 
 const dependenciesToGraphData = (deps: { [key: string]: string[] }) => {
   const nodes: DependencyNode[] = [];
