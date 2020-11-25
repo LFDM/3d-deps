4f6f71c20d73b1c1bb60fa799d3b08beec769416
LFDM <1986gh@gmail.com>
2020-11-25 19:01:52 +0100

    Remove unused code

--- src/components/Hud/NodesPanel.tsx
+++ src/components/Hud/NodesPanel.tsx
@@ -2,22 +2,6 @@ import { sortBy } from "lodash";
 import React, { useMemo } from "react";
 import { GraphData } from "../../types/GraphData";
 import { FileTreeDirectoryContent, toFileTree } from "../FileTree";
-/*
-
-      {treeNodes.map((t) => (
-        <Button
-          variant="none"
-          key={t.node.id}
-          fullWidth
-          onClick={() =>
-            setSelectedNodeId(t.node.id === selectedNodeId ? null : t.node.id)
-          }
-        >
-          {t.node.label}
-        </Button>
-      ))}
-
-*/
 
 export const NodesPanel = ({
   g,
