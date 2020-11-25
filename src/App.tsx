b80c7bc75f79f5639046c6961615a9a356a104d0
LFDM <1986gh@gmail.com>
2020-11-25 06:35:39 +0100

    Start to implement data hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import "./App.css";
 import { Config } from "./types/Config";
 import { DependencyNode } from "./types/DependencyAnalyzer";
-import { IGraphNode } from "./types/GraphData";
+import { GraphData, IGraphLink, IGraphNode } from "./types/GraphData";
+
+const depsToGraphData = (ds: DependencyNode[]): GraphData => {
+  const nodes: IGraphNode[] = [];
+  const links: IGraphLink[] = [];
+  ds.forEach((n) => {
+    const node: IGraphNode = {
+      id: n.id,
+      label: n.label || n.id,
+      path: n.path,
+    };
+    nodes.push(node);
+    n.dependsOn.forEach((v) => {
+      const link: IGraphLink = {
+        source: n.id,
+        target: v,
+      };
+      links.push(link);
+    });
+  });
+  return { nodes, links };
+};
+
+const useGraphData = (ds: DependencyNode[]) => {
+  return useMemo(() => {
+    return {
+      graphData: depsToGraphData(ds),
+    };
+  }, [ds]);
+};
 
 const Graph = ({ ds }: { ds: DependencyNode[] }) => {
   // TODO
@@ -17,25 +46,26 @@ const Graph = ({ ds }: { ds: DependencyNode[] }) => {
   // - incoming deps -> 2-3 layers
   // - outgoing deps -> 2-3 layers
   // - all links between them, activate particles
+  const { graphData } = useGraphData(ds);
   const [i, setI] = useState(0);
   useEffect(() => {
     setInterval(() => {
-      setI(Math.floor(Math.random() * d.nodes.length));
+      setI(Math.floor(Math.random() * graphData.nodes.length));
     }, 500);
   }, []);
   return (
     <ForceGraph3D
-      graphData={d}
+      graphData={graphData}
       nodeId="id"
       nodeColor={(node: any) => {
-        if (d.nodes[i] === node) {
+        if (graphData.nodes[i] === node) {
           return "red";
         }
         return "";
       }}
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
-      nodeLabel={(node) => (node as IGraphNode).id}
+      nodeLabel={(node) => (node as IGraphNode).label}
       enableNodeDrag={false}
     />
   );
