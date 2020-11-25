499551496a4be288663e1decd83d5a27de74b895
LFDM <1986gh@gmail.com>
2020-11-25 16:07:44 +0100

    Add color picer

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,4 +1,43 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
 import { ChromePicker } from "react-color";
+
+const Container = styled("div")((p) => ({
+  position: "relative",
+  cursor: "pointer",
+}));
+
+const Row = styled("div")((p) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  "> :not(:first-child)": {
+    marginLeft: p.theme.spacing(2),
+  },
+}));
+
+const ColorName = styled("div")((p) => ({
+  display: "flex",
+  alignItems: "center",
+
+  "> :not(:first-child)": {
+    marginLeft: p.theme.spacing(),
+  },
+}));
+
+const Swatch = styled("div")<{ value: string }>((p) => ({
+  width: p.theme.spacing(2),
+  height: p.theme.spacing(2),
+  backgroundColor: p.value,
+}));
+
+const PickerContainer = styled("div")((p) => ({
+  position: "absolute",
+  top: "100%",
+  right: 0,
+  zIndex: 5,
+}));
 
 export const ColorPicker = ({
   value,
@@ -9,15 +48,29 @@ export const ColorPicker = ({
   value: string;
   onChange: (nextValue: string) => void;
 }) => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      <div>
-        <label>{label}</label>
-      </div>
-      <ChromePicker
-        color={value}
-        onChangeComplete={(nextColor) => onChange(nextColor.hex)}
-      />
-    </div>
+    <>
+      <Container role="button" onClick={() => setOpen(true)}>
+        <Row>
+          <label>{label}</label>
+          <ColorName>
+            <Swatch value={value} />
+            <div>{value}</div>
+          </ColorName>
+        </Row>
+        {open && (
+          <PickerContainer>
+            <ChromePicker
+              color={value}
+              onChangeComplete={(nextColor) => {
+                onChange(nextColor.hex);
+                setOpen(false);
+              }}
+            />
+          </PickerContainer>
+        )}
+      </Container>
+    </>
   );
 };
