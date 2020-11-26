f7bffb4ee7152687ff1531765f0606ea9611247d
LFDM <1986gh@gmail.com>
2020-11-26 11:05:40 +0100

    Add overlay context

---
+++ src/components/Hud/OverlayContext.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useContext, useEffect, useState } from "react";
+
+export const OverlayContext = React.createContext<{
+  active: boolean;
+  setActive: (nextActive: boolean) => void;
+}>({
+  active: false,
+  setActive: () => undefined,
+});
+
+export const OverlayContextProvider: React.FC = ({ children }) => {
+  const [active, setActive] = useState(false);
+  // implement esc handling
+  return (
+    <OverlayContext.Provider value={{ active, setActive }}>
+      {children}
+    </OverlayContext.Provider>
+  );
+};
+
+export const useOverlayContext = () => {
+  const { active, setActive } = useContext(OverlayContext);
+  return [active, setActive] as const;
+};
+
+export const usePopupState = (initialState: boolean = false) => {
+  const [open, _setOpen] = useState(initialState);
+  const { setActive } = useContext(OverlayContext);
+  useEffect(() => {
+    if (initialState) {
+      setActive(true);
+    }
+  }, []);
+  const setOpen = useCallback((nextOpen: boolean) => {
+    _setOpen(nextOpen);
+    setActive(nextOpen);
+  }, []);
+
+  return [open, setOpen] as const;
+};
