5909d3a440b168e728fcdc1a29e5bf6fba7a1dff
LFDM <1986gh@gmail.com>
2020-11-27 18:27:20 +0100

    Add particles

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -142,6 +142,7 @@ export const Graph = ({
       Object.entries(dependsOn.links).forEach(([linkId, level]) => {
         addLinkStyle(ss, linkId, {
           color: colorForLevel(linkDependentColor, level),
+          particles: Math.max(0, Math.round(7 - level * 3)),
         });
       });
 
@@ -149,6 +150,7 @@ export const Graph = ({
       Object.entries(dependedBy.links).forEach(([linkId, level]) => {
         addLinkStyle(ss, linkId, {
           color: colorForLevel(linkDepdendencyColor, level),
+          particles: Math.max(0, Math.round(7 - level * 3)),
         });
       });
 
