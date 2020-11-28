deb7b56ad65c31dd9d3fdbb24ad96766048a488c
LFDM <1986gh@gmail.com>
2020-11-28 16:09:31 +0100

    Simplify a bit

--- src/components/Hotkeys.tsx
+++ src/components/Hotkeys.tsx
@@ -17,68 +17,67 @@ type KeyHandlers = {
   [K in Hotkey]: (ev?: KeyboardEvent) => void;
 };
 
-const toKeyMap = (hotkeys: HotkeyConfig): KeyMap => {
+const toSequence = (
+  hotkeys: HotkeyConfig,
+  key: Hotkey
+): Omit<ExtendedKeyMapOptions, "sequence"> => {
   return {
-    "hud.sidebar.toggle": {
-      name: "hud.sidebar.toggle",
-      sequences: hotkeys["hud.sidebar.toggle"],
-      action: "keydown",
-    },
-    "hud.sidebar.openNodesPanel": {
-      name: "hud.sidebar.openNodesPanel",
-      sequences: hotkeys["hud.sidebar.openNodesPanel"],
-      action: "keydown",
-    },
-    "hud.sidebar.openConfigPanel": {
-      name: "hud.sidebar.openConfigPanel",
-      sequences: hotkeys["hud.sidebar.openConfigPanel"],
-      action: "keydown",
-    },
-    "hud.search": {
-      name: "hud.search",
-      sequences: hotkeys["hud.search"],
-      action: "keydown",
-    },
-    "hud.hotkeyInfo": {
-      name: "hud.hotkeyInfo",
-      sequences: hotkeys["hud.hotkeyInfo"],
-      action: "keydown",
-    },
-    "graph.dependencies.maxDepth.increase": {
-      name: "graph.dependencies.maxDepth.increase",
-      sequences: hotkeys["graph.dependencies.maxDepth.increase"],
-      action: "keydown",
-    },
-    "graph.dependencies.maxDepth.decrease": {
-      name: "graph.dependencies.maxDepth.decrease",
-      sequences: hotkeys["graph.dependencies.maxDepth.decrease"],
-      action: "keydown",
-    },
-    "graph.dependents.maxDepth.increase": {
-      name: "graph.dependents.maxDepth.increase",
-      sequences: hotkeys["graph.dependents.maxDepth.increase"],
-      action: "keydown",
-    },
-    "graph.dependents.maxDepth.decrease": {
-      name: "graph.dependents.maxDepth.decrease",
-      sequences: hotkeys["graph.dependents.maxDepth.decrease"],
-      action: "keydown",
-    },
-    "graph.selectedNode.exclude": {
-      name: "graph.selectedNode.exclude",
-      sequences: hotkeys["graph.selectedNode.exclude"],
-      action: "keydown",
-    },
-    "graph.selectedNode.toggleDetails": {
-      name: "graph.selectedNode.toggleDetails",
-      sequences: hotkeys["graph.selectedNode.toggleDetails"],
-      action: "keydown",
-    },
-    "graph.selectedNode.toggleSelection": {
-      name: "graph.selectedNode.toggleSelection",
-      sequences: hotkeys["graph.selectedNode.toggleSelection"],
-      action: "keydown",
-    },
+    name: key,
+    sequences: hotkeys[key],
+    action: "keydown",
+  };
+};
+
+const ALL_HOTKEYS: Hotkey[] = [
+  "hud.sidebar.toggle",
+  "hud.sidebar.openNodesPanel",
+  "hud.sidebar.openConfigPanel",
+  "hud.search",
+  "hud.hotkeyInfo",
+  "graph.dependencies.maxDepth.increase",
+  "graph.dependencies.maxDepth.decrease",
+  "graph.dependents.maxDepth.increase",
+  "graph.dependents.maxDepth.decrease",
+  "graph.selectedNode.exclude",
+  "graph.selectedNode.toggleDetails",
+  "graph.selectedNode.toggleSelection",
+];
+
+const toKeyMap = (hs: HotkeyConfig): KeyMap => {
+  return {
+    "hud.sidebar.toggle": toSequence(hs, "hud.sidebar.toggle"),
+    "hud.sidebar.openNodesPanel": toSequence(hs, "hud.sidebar.openNodesPanel"),
+    "hud.sidebar.openConfigPanel": toSequence(
+      hs,
+      "hud.sidebar.openConfigPanel"
+    ),
+    "hud.search": toSequence(hs, "hud.search"),
+    "hud.hotkeyInfo": toSequence(hs, "hud.hotkeyInfo"),
+    "graph.dependencies.maxDepth.increase": toSequence(
+      hs,
+      "graph.dependencies.maxDepth.increase"
+    ),
+    "graph.dependencies.maxDepth.decrease": toSequence(
+      hs,
+      "graph.dependencies.maxDepth.decrease"
+    ),
+    "graph.dependents.maxDepth.increase": toSequence(
+      hs,
+      "graph.dependents.maxDepth.increase"
+    ),
+    "graph.dependents.maxDepth.decrease": toSequence(
+      hs,
+      "graph.dependents.maxDepth.decrease"
+    ),
+    "graph.selectedNode.exclude": toSequence(hs, "graph.selectedNode.exclude"),
+    "graph.selectedNode.toggleDetails": toSequence(
+      hs,
+      "graph.selectedNode.toggleDetails"
+    ),
+    "graph.selectedNode.toggleSelection": toSequence(
+      hs,
+      "graph.selectedNode.toggleSelection"
+    ),
   };
 };
 
