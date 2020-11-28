120fce7b1a81a8ff4e9ee2ac8d1af6b449f3d2e5
LFDM <1986gh@gmail.com>
2020-11-28 15:39:41 +0100

    Close after making a selection

--- src/components/Hud/SearchModal.tsx
+++ src/components/Hud/SearchModal.tsx
@@ -1,20 +1,41 @@
 import React from "react";
 import { useUiState } from "../../services/uiState";
+import { Autocomplete } from "../Autocompete";
 import { Dialog, DialogTitle } from "../Dialog";
 
 export const SearchModal = () => {
   const [
     {
+      graph: { data: g },
       hud: {
         search: { open: searchOpen },
       },
     },
-    { setSearchOpen },
+    { setSearchOpen, setSelectedNodeId },
   ] = useUiState();
   const close = () => setSearchOpen(false);
   return (
-    <Dialog open={searchOpen} onClose={close}>
+    <Dialog open={searchOpen} onClose={close} width={700}>
       <DialogTitle>Search</DialogTitle>
+
+      <Autocomplete
+        items={g.list}
+        renderItem={(t) => (t.exclude ? <s>{t.path}</s> : t.path)}
+        itemToKey={(t) => t.id}
+        filterItems={(ts, v) => {
+          const re = new RegExp(v, "i");
+          return ts.filter((t) => re.test(t.path));
+        }}
+        onSelect={(t) => {
+          if (t.exclude) {
+            return;
+          }
+          setSelectedNodeId(t.id);
+          close();
+        }}
+        fullWidth
+        autoFocus
+      />
     </Dialog>
   );
 };
