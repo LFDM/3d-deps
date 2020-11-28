42a6d60376fec54f7a1c5317f444a823ea9404cb
LFDM <1986gh@gmail.com>
2020-11-28 10:44:33 +0100

    Define hud and hotkeys config

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -41,9 +41,30 @@ export type GraphConfig = {
   excludeByPath?: RegExp;
 };
 
+export type HudConfig = {
+  sidebar: {
+    open: boolean;
+  };
+};
+
+export type HotkeyConfig = {
+  "hud.sidebar.toggle": string[];
+  "hud.sidebar.openNodesPanel": string[];
+  "hud.sidebar.openConfigPanel": string[];
+  "hud.search": string[];
+  "graph.dependencies.maxDepth.increase": string[];
+  "graph.dependencies.maxDepth.decrease": string[];
+  "graph.dependents.maxDepth.increase": string[];
+  "graph.dependents.maxDepth.decrease": string[];
+  "graph.selectedNode.exclude": string[];
+  "graph.selectedNode.toggleDetails": string[];
+};
+
 export type Config = {
   theme: Theme;
   graph: GraphConfig;
+  hud: HudConfig;
+  hotkeys: HotkeyConfig;
 };
 
 export const CONFIG: Config = {
@@ -87,5 +108,22 @@ export const CONFIG: Config = {
       maxDepth: 1,
     },
     excludeByPath: /(^helpers.ts|^domainTypes|^versions|^services\/firebase)/,
+  },
+  hud: {
+    sidebar: {
+      open: true,
+    },
+  },
+  hotkeys: {
+    "hud.sidebar.toggle": ["alt+b"],
+    "hud.sidebar.openNodesPanel": ["alt+n"],
+    "hud.sidebar.openConfigPanel": ["alt+c"],
+    "hud.search": ["/", "f"],
+    "graph.dependencies.maxDepth.increase": ["ctrl+j", "ctrl+up"],
+    "graph.dependencies.maxDepth.decrease": ["ctrl+k", "ctrl+down"],
+    "graph.dependents.maxDepth.increase": ["ctrl+alt+k", "ctrl+alt+down"],
+    "graph.dependents.maxDepth.decrease": ["ctrl+alt+k", "ctrl+alt+down"],
+    "graph.selectedNode.exclude": ["del"],
+    "graph.selectedNode.toggleDetails": ["d"],
   },
 };
