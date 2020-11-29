fc40a969b66efa0f67556708c4b4b5edb204d6c0
LFDM <1986gh@gmail.com>
2020-11-29 09:00:00 +0100

    Make hud segment use border box

--- src/components/Hud/HudSegment.tsx
+++ src/components/Hud/HudSegment.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 export const HudSegment = styled("div")`
+  box-sizing: border-box;
   overflow: auto;
   padding: ${(p) => p.theme.spacing(2)}px;
   pointer-events: auto;
