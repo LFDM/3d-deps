f75e2e2611b37bfcda4bffe7f9e9689334beb202
LFDM <1986gh@gmail.com>
2020-11-29 10:55:21 +0100

    Allow to make ConfigRow dense

--- src/components/ConfigRow.tsx
+++ src/components/ConfigRow.tsx
@@ -1,11 +1,11 @@
 import styled from "@emotion/styled";
 
-export const ConfigRow = styled("div")((p) => ({
+export const ConfigRow = styled("div")<{ dense?: boolean }>((p) => ({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
 
   "> :not(:first-child)": {
-    marginLeft: p.theme.spacing(2),
+    marginLeft: p.theme.spacing(p.dense ? 0.5 : 2),
   },
 }));
