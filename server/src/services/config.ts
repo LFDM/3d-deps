c50ac9ed56d40e1ebeb9d4f8358b43d28c8f0543
LFDM <1986gh@gmail.com>
2020-11-29 20:56:19 +0100

    Add config serializer

---
+++ server/src/services/config.ts
@@ -0,0 +1,35 @@
+import {
+  Config,
+  GraphConfig,
+  HotkeyConfig,
+  HudConfig,
+  Theme,
+} from "../types/Config";
+type Omit<T, K> = { [key in Exclude<keyof T, K>]: T[key] };
+
+// everything can be partial here too
+type SerializedConfig = {
+  theme: Omit<Theme, "spacing"> & { spacing: { unit: number } };
+  graph: Omit<GraphConfig, "excludeByPath" | "includeByPath"> & {
+    excludeByPath: string;
+    includeByPath: string;
+  };
+  hud: HudConfig;
+  hotkeys: HotkeyConfig;
+};
+
+export const serializeConfig = (conf: Config): SerializedConfig => {
+  return {
+    theme: {
+      ...conf.theme,
+      spacing: { unit: conf.theme.spacing(1) },
+    },
+    graph: {
+      ...conf.graph,
+      excludeByPath: conf.graph.excludeByPath?.toString() || "",
+      includeByPath: conf.graph.includeByPath?.toString() || "",
+    },
+    hud: conf.hud,
+    hotkeys: conf.hotkeys,
+  };
+};
