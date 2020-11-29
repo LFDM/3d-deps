f7c970f0edad76470bd88721bafb725ab6b30ac1
LFDM <1986gh@gmail.com>
2020-11-29 19:02:40 +0100

    Start the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import express from "express";
+import * as path from "path";
+
+const app = express();
+const PORT = 8000; // read from config file later
+
+const CLIENT_BUILD = path.join(__dirname, "..", "client_build");
+app.use(express.static(CLIENT_BUILD));
+
+app.get("/", (req, res) => res.redirect("/app"));
+app.get("/app/*", (req, res) => {
+  res.sendFile(path.join(CLIENT_BUILD, "index.html"));
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
