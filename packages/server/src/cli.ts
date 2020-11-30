fd6036438e26bf390e257c0c3a3763d7c819bfbb
LFDM <1986gh@gmail.com>
2020-11-30 07:44:36 +0100

    Rename bin - will later extract to own package

---
+++ packages/server/src/cli.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import * as path from "path";
+import yargs from "yargs";
+import { createServer } from ".";
+
+const argv = yargs(process.argv)
+  .option("port", {
+    alias: "p",
+    type: "number",
+  })
+  .option("config", {
+    alias: "c",
+    type: "string",
+  }).argv;
+
+// FIXME absolute paths!
+const configPath = path.join(process.cwd(), argv.config || "3d-deps.config.js");
+const port = argv.port || parseInt(process.env.PORT || "", 10) || 8000;
+
+const server = createServer(configPath);
+server.start(port);
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,22 +1,101 @@
-#!/usr/bin/env node
-
+import express from "express";
+import open from "open";
 import * as path from "path";
-import yargs from "yargs";
-import { createServer } from "./server";
+import { serializeConfig } from "./services/config";
+import { Dataset, RunConfig } from "./types/RunConfig";
 
-const argv = yargs(process.argv)
-  .option("port", {
-    alias: "p",
-    type: "number",
-  })
-  .option("config", {
-    alias: "c",
-    type: "string",
-  }).argv;
+const readConfig = (cfgPath: string): RunConfig | null => {
+  try {
+    return require(cfgPath);
+  } catch {
+    return null;
+  }
+};
 
-// FIXME absolute paths!
-const configPath = path.join(process.cwd(), argv.config || "3d-deps.config.js");
-const port = argv.port || parseInt(process.env.PORT || "", 10) || 8000;
+type Server = {
+  start: (port: number | string, openBrowser?: boolean) => Promise<void>;
+};
 
-const server = createServer(configPath);
-server.start(port);
+export const createServer = (configPath: string): Server => {
+  const app = express();
+
+  const CLIENT_BUILD = path.join(__dirname, "..", "..", "client_build");
+  app.use(express.static(CLIENT_BUILD));
+
+  app.get("/", (req, res) => res.redirect("/app"));
+
+  const DATASETS: { [key: string]: Dataset } = {};
+
+  app.get("/api/datasets", async (req, res) => {
+    const conf = readConfig(configPath);
+
+    if (!conf) {
+      res.status(400).json({
+        // TODO extract error codes and provide them to the client
+        message: "NO_CONFIG",
+      });
+      return;
+    }
+    if (!conf.loadDatasets) {
+      res.status(400).json({
+        message: "ILLEGAL_CONFIG",
+      });
+      return;
+    }
+    if (typeof conf.version === undefined) {
+      res.status(400).json({
+        message: "VERSION_MISSING",
+      });
+      return;
+    }
+    try {
+      const datasets = await conf.loadDatasets();
+      const datasetsWithId = datasets.map((d) => ({
+        id: Buffer.from(d.name).toString("base64"),
+        d,
+      }));
+      datasetsWithId.forEach((d) => (DATASETS[d.id] = d.d));
+      res.json(datasetsWithId.map((d) => ({ id: d.id, name: d.d.name })));
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: "FAILED_TO_LOAD_DATASETS",
+      });
+    }
+  });
+
+  app.get("/api/datasets/:id", async (req, res) => {
+    const dataset = DATASETS[req.params.id];
+    if (!dataset) {
+      res.status(404).json({ message: "DATA_SET_NOT_FOUND" });
+    }
+    const d = await dataset.fetch();
+    return res.json({
+      data: d.data,
+      config: serializeConfig(d.config),
+    });
+  });
+
+  app.get("/app/*", (req, res) => {
+    res.sendFile(path.join(CLIENT_BUILD, "index.html"));
+  });
+
+  const server: Server = {
+    start: (port, openBrowser) => {
+      return new Promise((resolve, reject) => {
+        try {
+          app.listen(port, () => {
+            const url = `http://localhost:${port}`;
+            console.log(`Server running at ${url}`);
+            openBrowser && open(url);
+            resolve();
+          });
+        } catch (err) {
+          reject(err);
+        }
+      });
+    },
+  };
+
+  return server;
+};
