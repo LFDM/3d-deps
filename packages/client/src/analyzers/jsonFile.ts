3a65b18ff0dae2aaac4a86a77b3e80503fa5e6c1
LFDM <1986gh@gmail.com>
2020-11-30 18:38:19 +0100

    Handle node modules

--- packages/client/src/analyzers/jsonFile.ts
+++ packages/client/src/analyzers/jsonFile.ts
@@ -6,25 +6,73 @@ import {
 } from "../types/DependencyAnalyzer";
 // import DEPS from "./dependencies-affilimate-cli.json";
 
-const dependenciesToGraphData = (deps: { [key: string]: string[] }) => {
+type MadgeTree = { [key: string]: string[] };
+
+export const _toNodeModule = (t: string): string | null => {
+  if (!t.includes("node_modules")) {
+    return null;
+  }
+  const parts = t.split("/");
+  const res: string[] = [];
+  let foundRoot = false;
+  let isScoped = false;
+  for (let i = 0; i < parts.length; i++) {
+    const part = parts[i];
+    if (!foundRoot && part !== "node_modules" && !part.match(/^\.+$/)) {
+      return null;
+    }
+    if (part === "node_modules") {
+      foundRoot = true;
+      continue;
+    }
+    if (foundRoot) {
+      if (part === "@types") {
+        continue;
+      }
+      if (part.startsWith("@")) {
+        isScoped = true;
+        res.push(part);
+        continue;
+      }
+      if (isScoped && res.length === 1) {
+        res.push(part);
+      }
+      if (res.length === 0) {
+        res.push(part);
+        continue;
+      }
+      if (res.length === 2) {
+        break;
+      }
+    }
+  }
+  return res.length ? `node_modules/${res.join("/")}` : null;
+};
+
+export const _madgeTreeToNodes = (tree: MadgeTree): DependencyNode[] => {
   const nodes: DependencyNode[] = [];
-  const set: Set<string> = new Set();
-  Object.entries(deps).forEach(([k, vs]) => {
-    const node: DependencyNode = {
+  const nodeModules: Set<string> = new Set();
+  Object.entries(tree).forEach(([k, vs]) => {
+    nodes.push({
       id: k,
       path: k,
       label: k,
-      dependsOn: vs.filter((v) => {
-        if (v.includes("node_modules")) {
-          set.add(v);
-          return false;
+      dependsOn: vs.map((v) => {
+        const nodeModule = _toNodeModule(v);
+        if (nodeModule && !nodeModules.has(nodeModule)) {
+          nodeModules.add(nodeModule);
+          nodes.push({
+            id: nodeModule,
+            path: nodeModule,
+            label: nodeModule,
+            dependsOn: [],
+          });
+          return nodeModule;
         }
-        return true;
+        return nodeModule || v;
       }),
-    };
-    nodes.push(node);
+    });
   });
-  console.log(set);
   return nodes;
 };
 
@@ -63,6 +111,6 @@ export class JsonFileAnalyzer implements IDependencyAnalyzer {
     if (!x) {
       throw new Error("UNKNWON_FILE");
     }
-    return dependenciesToGraphData(await x());
+    return _madgeTreeToNodes(await x());
   }
 }
