f3b9975bf6d9d25439f839c201f2e2d0e65b29bd
LFDM <1986gh@gmail.com>
2020-11-30 07:59:43 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-cd client
-yarn build
-cd ..
-rm -rf server/client_build
-cp -r client/build server/client_build
+yarn client build
+rm -rf packages/server/client_build
+cp -r packages/client/build packages/server/client_build
