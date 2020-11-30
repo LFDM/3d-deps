b29a435dcf3c90e8cb8be99bb4748a20493fae40
LFDM <1986gh@gmail.com>
2020-11-30 09:55:10 +0100

    Fix example

--- packages/examples/simple/src/index.ts
+++ packages/examples/simple/src/index.ts
@@ -1,7 +1,4 @@
 import { A } from "./a";
-import { AA } from "./a/aa";
-import { AB } from "./a/ab";
-import { ABC } from "./a/ab/abc";
 import { B } from "./b";
 
-console.log(A, AA, AB, ABC, B);
+console.log(A, B);
