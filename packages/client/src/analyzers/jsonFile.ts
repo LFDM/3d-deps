4a7b747ffbe0615040abeacf7482098c96be88dc
LFDM <1986gh@gmail.com>
2020-11-30 14:33:32 +0100

    Add axios and express

--- packages/client/src/analyzers/jsonFile.ts
+++ packages/client/src/analyzers/jsonFile.ts
@@ -35,6 +35,8 @@ export const FILES: {
     import("./dependencies-syndexioi-cf.json").then((f) => f.default),
   "Material UI": () => import("./dependencies-mui.json").then((f) => f.default),
   Webpack: () => import("./dependencies-webpack.json").then((f) => f.default),
+  Axios: () => import("./dependencies-axios.json").then((f) => f.default),
+  Express: () => import("./dependencies-express.json").then((f) => f.default),
   Self: () => import("./dependencies-self.json").then((f) => f.default),
 };
 
