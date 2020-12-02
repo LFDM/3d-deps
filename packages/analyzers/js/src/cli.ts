9eb3d8b1a17b7b7cff8e07c661833954f38fa2e2
LFDM <1986gh@gmail.com>
2020-12-02 10:16:54 +0100

    Update cli

--- packages/analyzers/js/src/cli.ts
+++ packages/analyzers/js/src/cli.ts
@@ -1,14 +1,13 @@
 import * as path from "path";
 import { JsAnalyzer } from ".";
-import { TRANSFORMERS } from "./transformers";
 
 const analyzer = new JsAnalyzer({
   // rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "babel"),
-  // rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "react"),
-  rootDir: path.join(__dirname, "..", "..", "..", ".."),
-  configTransformer: TRANSFORMERS.MAP_ENTRY((entry) =>
-    entry.replace("dist/index.js", "src/index.ts")
-  ),
+  rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "react"),
+  // rootDir: path.join(__dirname, "..", "..", "..", ".."),
+  // configTransformer: TRANSFORMERS.MAP_ENTRY((entry) =>
+  //   entry.replace("dist/index.js", "src/index.ts")
+  // ),
 });
 
 analyzer.analyze().then((res) => console.log(JSON.stringify(res, null, 2)));
