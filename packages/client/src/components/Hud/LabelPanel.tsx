9733e6e0253d7156ef5f91c4526e36cf1120e25b
LFDM <1986gh@gmail.com>
2020-12-03 20:24:34 +0100

    Add controls and fix stling in labels panel

--- packages/client/src/components/Hud/LabelPanel.tsx
+++ packages/client/src/components/Hud/LabelPanel.tsx
@@ -1,6 +1,19 @@
+import styled from "@emotion/styled";
 import React from "react";
 import { useUiState } from "../../services/uiState";
+import { Button } from "../Button";
 import { SelectableNodeLabelChip } from "./NodeLabelChip";
+import {
+  SidebarPanelBody,
+  SidebarPanelContainer,
+  SidebarPanelFooter,
+} from "./SidebarPanelLayout";
+
+const ControlsGrid = styled("div")((p) => ({
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gridColumnGap: p.theme.spacing(3),
+}));
 
 export const LabelPanel = () => {
   const [
@@ -10,16 +23,24 @@ export const LabelPanel = () => {
     { toggleLabel },
   ] = useUiState();
   return (
-    <>
-      <>
-        {Object.values(labels).map((l) => (
-          <SelectableNodeLabelChip
-            key={l.key}
-            d={l}
-            onChange={(active) => toggleLabel(l.key, active)}
-          />
-        ))}
-      </>
-    </>
+    <SidebarPanelContainer>
+      <SidebarPanelBody>
+        <div>
+          {Object.values(labels).map((l) => (
+            <SelectableNodeLabelChip
+              key={l.key}
+              d={l}
+              onChange={(active) => toggleLabel(l.key, active)}
+            />
+          ))}
+        </div>
+      </SidebarPanelBody>
+      <SidebarPanelFooter>
+        <ControlsGrid>
+          <Button fullWidth>Enable all</Button>
+          <Button fullWidth>Disable all</Button>
+        </ControlsGrid>
+      </SidebarPanelFooter>
+    </SidebarPanelContainer>
   );
 };
