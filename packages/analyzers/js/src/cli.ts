14bfbbb0229b8a2d34b414ef31eefab133784fba
LFDM <1986gh@gmail.com>
2020-12-03 17:51:10 +0100

    Update react example

--- packages/analyzers/js/src/cli.ts
+++ packages/analyzers/js/src/cli.ts
@@ -10,22 +10,22 @@ const analyzer = new JsAnalyzer({
 
   // rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "babel"),
 
-  // rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "react"),
-  // configTransformer: async (args) => {
-  //   const cfg = await TRANSFORMERS.DEFAULT()(args);
-  //   return {
-  //     ...cfg,
-  //     entries: {
-  //       ...cfg.entries,
-  //       main: cfg.entries.main?.replace("dist/", "src/") || "./index.js",
-  //     },
-  //   };
-  // },
+  rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "react"),
+  configTransformer: async (args) => {
+    const cfg = await TRANSFORMERS.DEFAULT()(args);
+    return {
+      ...cfg,
+      entries: {
+        ...cfg.entries,
+        main: cfg.entries.main?.replace("dist/", "src/") || "./index.js",
+      },
+    };
+  },
 
-  rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "next.js"),
-  configTransformer: TRANSFORMERS.MAP_ENTRY((e) =>
-    e.replace("dist/", "").replace(/.js$/, ".ts")
-  ),
+  // rootDir: path.join(__dirname, "..", "..", "..", "..", "..", "next.js"),
+  // configTransformer: TRANSFORMERS.MAP_ENTRY((e) =>
+  //   e.replace("dist/", "").replace(/.js$/, ".ts")
+  // ),
 });
 
 analyzer.analyze().then((res) => console.log(JSON.stringify(res, null, 2)));
