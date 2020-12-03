11dc38d7474b4bd90050c365b5d8fdc61647d145
LFDM <1986gh@gmail.com>
2020-12-03 19:24:05 +0100

    Expose label darkening amount

--- packages/client/src/components/Hud/NodeLabelChip.tsx
+++ packages/client/src/components/Hud/NodeLabelChip.tsx
@@ -2,8 +2,10 @@ import styled from "@emotion/styled";
 import tinycolor from "tinycolor2";
 import { NodeLabel } from "../../services/uiState";
 
+const LABEL_DARKENING_AMOUNT = 50;
+
 const Chip = styled("div")<{ color: string; active: boolean }>((p) => ({
-  color: tinycolor(p.color).darken(50).toRgbString(),
+  color: tinycolor(p.color).darken(LABEL_DARKENING_AMOUNT).toRgbString(),
   backgroundColor: p.color,
   padding: p.theme.spacing(0.5),
   borderRadius: p.theme.spacing(0.25),
