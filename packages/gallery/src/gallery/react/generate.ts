e7e0325b632785bced55cfeddba371ebc0d4af0f
LFDM <1986gh@gmail.com>
2020-12-04 15:07:39 +0100

    Use the utils

--- packages/gallery/src/gallery/react/generate.ts
+++ packages/gallery/src/gallery/react/generate.ts
@@ -1,17 +1,11 @@
 #!/usr/bin/env node
 
 import { JsAnalyzer, TRANSFORMERS } from "@3d-deps/analyzer-js";
-import * as fs from "fs";
 import * as path from "path";
-import * as util from "util";
-
-const writeFile = util.promisify(fs.writeFile);
-
-const DIR = __dirname;
-const p = process.argv[2];
+import { getRootDir, writeJsonFile } from "../util";
 
 const analyzer = new JsAnalyzer({
-  rootDir: path.isAbsolute(p) ? p : path.join(DIR, process.argv[2]),
+  rootDir: getRootDir(process.argv),
   configTransformer: async (args) => {
     const cfg = await TRANSFORMERS.DEFAULT()(args);
     return {
@@ -26,6 +20,4 @@ const analyzer = new JsAnalyzer({
 
 analyzer
   .analyze()
-  .then((res) =>
-    writeFile(path.join(DIR, "data.json"), JSON.stringify(res, null, 2))
-  );
+  .then((res) => writeJsonFile(path.join(__dirname, "data.json"), res));
