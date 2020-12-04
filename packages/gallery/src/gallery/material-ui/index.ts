7fa7de6c4d96cd1209825d1329859ca49a1859ed
LFDM <1986gh@gmail.com>
2020-12-04 16:10:05 +0100

    Fix material ui

--- packages/gallery/src/gallery/material-ui/index.ts
+++ packages/gallery/src/gallery/material-ui/index.ts
@@ -2,12 +2,11 @@ import { Dataset, mergeWithDefaultConfig } from "@3d-deps/core";
 
 export const MATERIAL_UI: Dataset = {
   name: "Material UI",
-  icon:
-    "https://camo.githubusercontent.com/58423e406b227112756822122631d9eca5ab83334a6f0d8f2a6305b086815747/68747470733a2f2f6d6174657269616c2d75692e636f6d2f7374617469632f6c6f676f2e73766",
+  icon: "https://material-ui.com/static/logo_raw.svg",
   fetch: async () => ({
     config: mergeWithDefaultConfig({
       graph: {
-        excludeByPath: /((^|\/)node_modules\/)/,
+        excludeByPath: /((^|\/)node_modules\/|packages\/material-ui-icons)/,
       },
     }),
     data: (await import("./data.json")).default,
