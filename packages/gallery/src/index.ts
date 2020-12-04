3dacac83ed5518481091fa8a87e91795395e068e
LFDM <1986gh@gmail.com>
2020-12-04 15:30:45 +0100

    Add nivo

--- packages/gallery/src/index.ts
+++ packages/gallery/src/index.ts
@@ -1,9 +1,11 @@
 import { RunConfig } from "@3d-deps/core";
+import { NIVO } from "./gallery/nivo";
 import { REACT } from "./gallery/react";
+import { WEBPACK } from "./gallery/webpack";
 
 export const getConfig = (): RunConfig => {
   return {
     version: 1,
-    loadDatasets: async () => [REACT],
+    loadDatasets: async () => [NIVO, REACT, WEBPACK],
   };
 };
