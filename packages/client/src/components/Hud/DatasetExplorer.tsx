690f9df47c8ab838dc889935af6855f9f5ffda5b
LFDM <1986gh@gmail.com>
2020-12-04 11:40:52 +0100

    Add a button to open the browser

--- packages/client/src/components/Hud/DatasetExplorer.tsx
+++ packages/client/src/components/Hud/DatasetExplorer.tsx
@@ -1,6 +1,10 @@
+import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
 import React from "react";
+import { Maximize } from "react-feather";
+import { Link } from "react-router-dom";
 import { useDatasets } from "../../services/dataset";
+import { Button } from "../Button";
 import { Select } from "../Select";
 import { HudSegment } from "./HudSegment";
 
@@ -9,15 +13,29 @@ const Container = styled(HudSegment)((p) => ({
   top: 0,
   right: 0,
   padding: p.theme.spacing(2),
+
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+
+  "> :not(:first-child)": {
+    marginLeft: p.theme.spacing(),
+  },
 }));
 
 export const DatasetExplorer = () => {
   const { current, datasets, selectDataset } = useDatasets();
+  const t = useTheme();
   if (datasets.length < 2) {
     return null;
   }
   return (
     <Container>
+      <Link to="/browser" title="Open browser">
+        <Button variant="icon">
+          <Maximize size={16} color={t.hud.color} />
+        </Button>
+      </Link>
       <Select
         value={current.name}
         onChange={(ev) => {
