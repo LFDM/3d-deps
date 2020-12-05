f2b970e1e51f37e09e6019bf7a23a7c01f0fd442
LFDM <1986gh@gmail.com>
2020-12-05 18:03:33 +0100

    Add read file helper

--- packages/gallery/src/gallery/util.ts
+++ packages/gallery/src/gallery/util.ts
@@ -8,6 +8,9 @@ export const getRootDir = (argv: string[]) => {
   return path.isAbsolute(p) ? p : path.join(process.cwd(), process.argv[2]);
 };
 
+export const readFile = util.promisify(fs.readFile);
+export const readJsonFile = <T = any>(p: string) =>
+  readFile(p).then((o) => JSON.parse(p.toString()) as T);
 export const writeFile = util.promisify(fs.writeFile);
 export const writeJsonFile = (p: string, obj: any) =>
   writeFile(p, JSON.stringify(obj, null, 2));
