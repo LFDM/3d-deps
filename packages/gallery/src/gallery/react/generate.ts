638ee496319c526bc240809f079eb1659c2238e3
LFDM <1986gh@gmail.com>
2020-12-05 09:10:34 +0100

    Glob all shared files for react

--- packages/gallery/src/gallery/react/generate.ts
+++ packages/gallery/src/gallery/react/generate.ts
@@ -2,12 +2,20 @@
 
 import { JsAnalyzer, TRANSFORMERS } from "@3d-deps/analyzer-js";
 import * as path from "path";
-import { getRootDir, writeJsonFile } from "../util";
+import { getRootDir, glob, writeJsonFile } from "../util";
 
 const analyzer = new JsAnalyzer({
   rootDir: getRootDir(process.argv),
   configTransformer: async (args) => {
     const cfg = await TRANSFORMERS.DEFAULT()(args);
+    if (args.packageJson.name === "shared") {
+      cfg.entries = (
+        await glob("*.js", {
+          cwd: args.dir,
+        })
+      ).map((p) => ({ type: undefined, path: p }));
+      return cfg;
+    }
     const main = cfg.entries.find((e) => e.type === "main");
     if (main) {
       main.path = main.path.replace("dist", "src");
