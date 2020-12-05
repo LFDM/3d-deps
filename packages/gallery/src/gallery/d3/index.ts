7aaaf449f290eaaed5f9c66eabb57d2d9c2b8389
LFDM <1986gh@gmail.com>
2020-12-05 20:11:56 +0100

    Add D3 icon

--- packages/gallery/src/gallery/d3/index.ts
+++ packages/gallery/src/gallery/d3/index.ts
@@ -1,7 +1,9 @@
 import { Dataset, mergeWithDefaultConfig } from "@3d-deps/core";
 
 export const D3: Dataset = {
-  name: "d3",
+  name: "D3",
+  icon:
+    "https://camo.githubusercontent.com/586ccf0aad9684edc821658cee04146cf36d1f1d5ec904bbefd72728909ccb2e/68747470733a2f2f64336a732e6f72672f6c6f676f2e737667",
   fetch: async () => ({
     config: mergeWithDefaultConfig({
       graph: {
