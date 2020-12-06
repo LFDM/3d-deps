6ae16abcfbcc28b544606a154874032471bc4832
LFDM <1986gh@gmail.com>
2020-12-06 08:21:33 +0100

    Update 3d-deps generate conf

--- packages/gallery/src/gallery/3d-deps/generate.ts
+++ packages/gallery/src/gallery/3d-deps/generate.ts
@@ -1,15 +1,37 @@
 #!/usr/bin/env node
 
-import { JsAnalyzer, TRANSFORMERS } from "@3d-deps/analyzer-js";
+import { getTsCompilerOptions, JsAnalyzer } from "@3d-deps/analyzer-js";
 import * as path from "path";
 import { getRootDir, writeJsonFile } from "../util";
 
 const analyzer = new JsAnalyzer({
   rootDir: getRootDir(process.argv),
-  configTransformer: TRANSFORMERS.MAP_ENTRY((entry) => {
-    entry.path = entry.path.replace("dist", "src").replace("js", "ts");
-    return entry;
-  }),
+  configTransformer: async ({ packageJson }) => {
+    if (!packageJson.main) {
+      return {
+        entries: [],
+        configs: {},
+      };
+    }
+
+    return {
+      entries: [
+        {
+          type: "main",
+          path: packageJson.main.replace("dist", "src").replace("js", "ts"),
+        },
+      ],
+      configs: {
+        ts: {
+          compilerOptions: getTsCompilerOptions("tsconfig.json"),
+        },
+      },
+      cleanupPath: (p) =>
+        p.startsWith("dist")
+          ? p.replace("dist", "src").replace(".d.ts", ".ts")
+          : p,
+    };
+  },
 });
 
 analyzer
