95a02a1439bed7c82d004a5325a635d91c6dfb6f
LFDM <1986gh@gmail.com>
2020-12-06 09:53:13 +0100

    Simplify d3 generate

--- packages/gallery/src/gallery/d3/generate.ts
+++ packages/gallery/src/gallery/d3/generate.ts
@@ -2,40 +2,35 @@
 
 import { JsAnalyzer } from "@3d-deps/analyzer-js";
 import * as path from "path";
-import { getRootDir, readJsonFile, writeJsonFile } from "../util";
+import { getRootDir, writeJsonFile } from "../util";
 
-const generate = async () => {
-  const rootDir = getRootDir(process.argv);
-  const packageJson = await readJsonFile(path.join(rootDir, "package.json"));
-  const d3Packages = Object.keys(packageJson.dependencies).filter((k) =>
-    k.startsWith("d3")
-  );
-
-  const analyzer = new JsAnalyzer({
-    rootDir,
-    configTransformer: async ({ packageJson }) => {
-      return {
-        entries: [
-          {
-            type: "main",
-            path: packageJson.module || "./src/index.js",
-          },
-        ],
-        configs: {},
-        cleanupPath: (p, pkg) => (p === pkg.main ? pkg.module || p : p),
-      };
-    },
-    workspaces: {
-      virtual: d3Packages.map((packageName) => ({
+const analyzer = new JsAnalyzer({
+  rootDir: getRootDir(process.argv),
+  configTransformer: async ({ packageJson }) => {
+    return {
+      entries: [
+        {
+          type: "main",
+          path: packageJson.module || "./src/index.js",
+        },
+      ],
+      configs: {},
+      cleanupPath: (p, pkg) => (p === pkg.main ? pkg.module || p : p),
+    };
+  },
+  workspaces: {
+    virtual: async (packageJson) => {
+      const d3Packages = Object.keys(packageJson.dependencies).filter((k) =>
+        k.startsWith("d3")
+      );
+      return d3Packages.map((packageName) => ({
         packageName,
         mountPoint: "packages",
-      })),
+      }));
     },
-  });
+  },
+});
 
-  analyzer
-    .analyze()
-    .then((res) => writeJsonFile(path.join(__dirname, "data.json"), res));
-};
-
-generate();
+analyzer
+  .analyze()
+  .then((res) => writeJsonFile(path.join(__dirname, "data.json"), res));
