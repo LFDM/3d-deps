c829c04c4947d60dd1f919344854b04bf22a562e
LFDM <1986gh@gmail.com>
2020-12-06 11:50:52 +0100

    Much improved tensorflow version

--- packages/gallery/src/gallery/tfjs/generate.ts
+++ packages/gallery/src/gallery/tfjs/generate.ts
@@ -2,41 +2,78 @@
 
 import { getTsCompilerOptions, JsAnalyzer } from "@3d-deps/analyzer-js";
 import * as path from "path";
-import { getRootDir, writeJsonFile } from "../util";
+import { getRootDir, readJsonFile, writeJsonFile } from "../util";
 
-const analyzer = new JsAnalyzer({
-  rootDir: getRootDir(process.argv),
-  configTransformer: () => {
-    return {
-      entries: [
-        {
-          type: "main",
-          path: "./src/index.ts",
-        },
-      ],
-      configs: {
-        ts: {
-          compilerOptions: getTsCompilerOptions("tsconfig.json"),
-        },
-      },
-      cleanupPath: (p) =>
-        p.startsWith("dist")
-          ? p.replace("dist", "src").replace(".d.ts", ".ts")
-          : p,
-    };
-  },
-  workspaces: {
-    virtual: async ({ rootPkg }) => {
-      return Object.entries<string>(rootPkg.dependencies)
+const generate = async () => {
+  const rootDir = getRootDir(process.argv);
+  const topLevelPackages = ["tfjs", "tfjs-node", "tfjs-react-native"];
+  const virtualWorkspaces = await Promise.all(
+    topLevelPackages.map(async (name) => {
+      const packageJson = await readJsonFile(
+        path.join(rootDir, name, "package.json")
+      );
+      const tfPackages = Object.entries<string>(packageJson.dependencies)
         .filter(([k]) => k.startsWith("@tensorflow"))
         .map(([k, v]) => ({
           packageName: k,
-          mountPoint: v.replace("link:..", ""),
+          location: v.replace("link:../", ""),
+          mountPoint: v.replace("link:../", ""), // path.join("packages", v.replace("link:..", "")),
         }));
-    },
-  },
-});
+      return [
+        {
+          packageName: packageJson.name,
+          location: name,
+          mountPoint: name,
+        },
+        ...tfPackages,
+      ];
+    })
+  ).then((wss) => {
+    const byName: {
+      [key: string]: {
+        packageName: string;
+        location: string;
+        mountPoint: string;
+      };
+    } = {};
+    wss.forEach((ws) => ws.forEach((w) => (byName[w.packageName] = w)));
+    return Object.values(byName);
+  });
 
-analyzer
-  .analyze()
-  .then((res) => writeJsonFile(path.join(__dirname, "data.json"), res));
+  const analyzer = new JsAnalyzer({
+    rootDir,
+    configTransformer: ({ packageJson }) => {
+      if (!packageJson.name) {
+        return {
+          entries: [],
+        };
+      }
+      return {
+        entries: [
+          {
+            type: "main",
+            path: "./src/index.ts",
+          },
+        ],
+        configs: {
+          ts: {
+            compilerOptions: getTsCompilerOptions("tsconfig.json"),
+          },
+        },
+        cleanupPath: (p) =>
+          p.startsWith("dist")
+            ? p.replace("dist", "src").replace(".d.ts", ".ts")
+            : p,
+      };
+    },
+    workspaces: {
+      virtual: virtualWorkspaces,
+    },
+  });
+
+  analyzer
+    .analyze()
+    .then((res) => writeJsonFile(path.join(__dirname, "data.json"), res));
+};
+
+generate();
